504bbc6f747f360d279ae9f98194ab42cd104689
Bleddyn Pijpers <bleddyn.pijpers@foursites.nl>
2021-12-10 00:06:31 +0800

    added icons

--- components/page-components/OurMethods.js
+++ components/page-components/OurMethods.js
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 // Icons
 import Sparkles from "@assets/icons/sparkles.svg";
+import Evaluate from "@assets/icons/evaluate.svg";
+import Pulse from "@assets/icons/pulse.svg";
+import Complete from "@assets/icons/complete.svg";
+
+
 
 const OurMethods = () => {
   return (
@@ -28,9 +33,9 @@ const OurMethods = () => {
             </div>
           </div>
           <div className="method">
-            <Image className="method__image" src={Sparkles} alt='sparlkes' width={300} height={300}/>
+            <Image className="method__image" src={Evaluate} alt='sparlkes' width={300} height={300}/>
             <div className="method__content">
-              <h4>Intake</h4>
+              <h4>Planning</h4>
               <p>
                 Wil jij het maximale uit jezelf halen? Mentaal sterker worden en
                 je prestaties verbeteren? Dan ben je hier op het juiste adres.
@@ -39,9 +44,9 @@ const OurMethods = () => {
             </div>
           </div>
           <div className="method">
-            <Image className="method__image" src={Sparkles} alt='sparlkes' width={300} height={300}/>
+            <Image className="method__image" src={Pulse} alt='sparlkes' width={300} height={300}/>
             <div className="method__content">
-              <h4>Intake</h4>
+              <h4>Training</h4>
               <p>
                 Wil jij het maximale uit jezelf halen? Mentaal sterker worden en
                 je prestaties verbeteren? Dan ben je hier op het juiste adres.
@@ -50,9 +55,9 @@ const OurMethods = () => {
             </div>
           </div>
           <div className="method">
-            <Image className="method__image" src={Sparkles} alt='sparlkes' width={300} height={300}/>
+            <Image className="method__image" src={Complete} alt='sparlkes' width={300} height={300}/>
             <div className="method__content">
-              <h4>Intake</h4>
+              <h4>Evaluatie</h4>
               <p>
                 Wil jij het maximale uit jezelf halen? Mentaal sterker worden en
                 je prestaties verbeteren? Dan ben je hier op het juiste adres.
