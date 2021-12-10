54a53c1a03ad310f97817f68333ebad0b2182a8d
Bleddyn Pijpers <bleddyn.pijpers@foursites.nl>
2021-12-10 18:44:40 +0800

    werkwijze mobile fix

--- pages/werkwijze/index.js
+++ pages/werkwijze/index.js
@@ -1,14 +1,11 @@
 // Components
-import Hero from "@components/ui-components/Hero";
+import NavBarBg from "@ui/layout/NavBarBg";
 import OurMethods from "@components/page-components/OurMethods";
 import MethodsServices from "@components/page-components/MethodsServices";
 import TextBlock from "@ui/TextBlock";
 import Footer from "@ui/layout/Footer";
 
 const Methods = () => {
-  const image =
-    "https://images.unsplash.com/photo-1483721310020-03333e577078?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3028&q=80";
-
   const location = {
     title: "Locatie",
     content:
@@ -23,7 +20,7 @@ const Methods = () => {
 
   return (
     <div>
-      <Hero image={image} title="Onze werkwijze" />
+      <NavBarBg />
       <OurMethods />
       <MethodsServices />
       <TextBlock data={location}/>
