740bcd83b897a19d4b33a662f58cb1bbf7e20363
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2022-01-09 20:15:21 +0100

    app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.scss";
 import { Fragment } from "react";
 
+// Typescript
+import type { AppProps } from 'next/app'
+
 // Components
 import Navbar from "@ui/layout/Navbar";
 import FooterStrip from '@ui/layout/FooterStrip'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Navbar />
