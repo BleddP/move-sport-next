5645354a65aa40acfe593487024e07fe1ba83788
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2022-01-26 18:22:22 +0100

    socials page

--- pages/socials.js
+++ pages/socials.js
@@ -5,7 +5,7 @@ import Clients from '@components/page-components/Clients'
 import PartnerStrip from '@ui/PartnerStrip'
 import Footer from '@ui/layout/Footer'
 
-const Socials = () => {
+const Socials = ({page}) => {
 
     const events = [
         {
@@ -58,13 +58,32 @@ const Socials = () => {
     return (
         <main>
             <NavBarBg />
-            <Services />
+            <Services services={page.services.services.data} />
             <Events events={events} />
-            <Clients />
-            <PartnerStrip />
+            <Clients clients={page.clients} />
+            <PartnerStrip partners={page.partners[0]} />
             <Footer/>
         </main>
     )
+}
+
+// Libs
+import axios from 'axios';
+
+// Fetch data from the server
+export async function getServerSideProps(context) {
+
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/homepage`)
+  
+  if (response.status && response.status < 300) {
+    return {
+      props: {
+        page: response.data.data.attributes
+      }
+    }
+  } else {
+    console.log('Error: ', response)
+  }
 }
 
 export default Socials
