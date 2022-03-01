bfda2c70e2770477ef771c304c9e5d598023ea39
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2022-03-01 21:34:10 +0800

    removed partners from aanbod page

--- pages/aanbod/[method].js
+++ pages/aanbod/[method].js
@@ -7,54 +7,56 @@ import Hero from "@components/ui-components/Hero";
 import Accordion from "@components/ui-components/Accordion";
 import Approaches from "@components/page-components/Approaches";
 import PartnerStrip from "@components/ui-components/PartnerStrip";
-import Footer from '@components/ui-components/layout/Footer'
+import Footer from "@components/ui-components/layout/Footer";
 
 // Animated wrapper
 import FadeIn from "@components/animated-components/FadeIn";
 
 const Method = ({ page }) => {
-
   const offers = [
     {
       id: 1,
-      title: 'Traject 1',
-      content: 'Some content',
-      image: 'https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+      title: "Traject 1",
+      content: "Some content",
+      image:
+        "https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
       button: {
-        to: '/contact',
-        text: 'Neem contact op',
-        type: 'primary',
-        target: 'internal'
-      }
+        to: "/contact",
+        text: "Neem contact op",
+        type: "primary",
+        target: "internal",
+      },
     },
     {
       id: 2,
-      title: 'Traject 2',
-      content: 'Some content',
-      image: 'https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+      title: "Traject 2",
+      content: "Some content",
+      image:
+        "https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
       button: {
-        to: '/contact',
-        text: 'Neem contact op',
-        type: 'primary',
-        target: 'internal'
-      }
+        to: "/contact",
+        text: "Neem contact op",
+        type: "primary",
+        target: "internal",
+      },
     },
     {
       id: 3,
-      title: 'Traject 3',
-      content: 'Some content',
-      image: 'https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+      title: "Traject 3",
+      content: "Some content",
+      image:
+        "https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
       button: {
-        to: '/contact',
-        text: 'Neem contact op',
-        type: 'primary',
-        target: 'internal'
-      }
+        to: "/contact",
+        text: "Neem contact op",
+        type: "primary",
+        target: "internal",
+      },
     },
-  ]
+  ];
 
-  page.header.title = 'Ons aanbod voor teams'
-  page.header.buttons = []
+  page.header.title = "Ons aanbod voor teams";
+  page.header.buttons = [];
 
   return (
     <div>
@@ -63,34 +65,37 @@ const Method = ({ page }) => {
       <main className="page-product">
         <div className="container">
           <FadeIn>
-          <section className="section">
-            <div className="columns columns--2-col">
-              <div>
-                <h1>Wat is het</h1>
-                <p>
-                  Sportpsychologie voor (sport) teams is vaak gericht op het
-                  verbeteren van het welzijn, procesverbetering en resultaat; de
-                  ontwikkeling van het team, het verbeteren van de samenwerking
-                  en communicatie, het vergroten van de mentale weerbaarheid en
-                  het verbeteren van de teamprestaties om op deze manier het
-                  beste uit alle teamleden en elkaar te kunnen halen.
-                </p>
-                <h2>Voorbeelden</h2>
-                <ul>
-                  <li>Meer dan 1 team willen worden</li>
-                  <li>Meer dan 1 team willen worden</li>
-                  <li>Als team kunnen samenwerken</li>
-                  <li>Inzicht krijgen in teamkwaliteiten en aandachtspunten</li>
-                </ul>
+            <section className="section">
+              <div className="columns columns--2-col">
+                <div>
+                  <h1>Wat is het</h1>
+                  <p>
+                    Sportpsychologie voor (sport) teams is vaak gericht op het
+                    verbeteren van het welzijn, procesverbetering en resultaat;
+                    de ontwikkeling van het team, het verbeteren van de
+                    samenwerking en communicatie, het vergroten van de mentale
+                    weerbaarheid en het verbeteren van de teamprestaties om op
+                    deze manier het beste uit alle teamleden en elkaar te kunnen
+                    halen.
+                  </p>
+                  <h2>Voorbeelden</h2>
+                  <ul>
+                    <li>Meer dan 1 team willen worden</li>
+                    <li>Meer dan 1 team willen worden</li>
+                    <li>Als team kunnen samenwerken</li>
+                    <li>
+                      Inzicht krijgen in teamkwaliteiten en aandachtspunten
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <img
+                    alt="image"
+                    src="https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+                  />
+                </div>
               </div>
-              <div>
-                <img
-                  alt="image"
-                  src="https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-                />
-              </div>
-            </div>
-          </section>
+            </section>
           </FadeIn>
 
           <section className="section">
@@ -129,7 +134,7 @@ const Method = ({ page }) => {
           </section>
         </div>
       </main>
-      <PartnerStrip partners={page.partners} />
+      {/* {page.partners && <PartnerStrip partners={page.partners} />} */}
       <Footer />
     </div>
   );
