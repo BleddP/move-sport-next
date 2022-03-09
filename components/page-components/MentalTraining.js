4ef93fe9d86d209f6f15c31300427dfbe43b81d2
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2022-03-09 13:09:42 +0800

    added unicode char replacements

--- components/page-components/MentalTraining.js
+++ components/page-components/MentalTraining.js
@@ -1,8 +1,6 @@
 import Image from "next/image";
 
 // Content
-import Pulse from "@assets/icons/pulse.svg";
-import Sparkles from "@assets/icons/sparkles.svg";
 import Complete from "@assets/icons/complete.svg";
 
 // Helper functions
@@ -14,6 +12,10 @@ const MentalTraining = ({data}) => {
   const image_2 = renderImage(data.image_2.data.attributes)
   const image_3 = renderImage(data.image_3.data.attributes)
 
+  const replaceLi = (content) => {
+    return content.replace(/- /g, '\u2714 ')
+  }
+
   return (
     <section className="mental-training">
       <div className="container container--96">
@@ -23,19 +25,19 @@ const MentalTraining = ({data}) => {
             <div className="usp">
               <Image width={96} height={96} src={image_1.url} />
               <p>
-                {data.intro_1}
+                {replaceLi(data.intro_1)}
               </p>
             </div>
             <div className="usp">
               <Image width={96} height={96} src={image_2.url} />
               <p>
-                {data.intro_2}
+                {replaceLi(data.intro_2)}
               </p>
             </div>
             <div className="usp">
               <Image width={96} height={96} src={image_3.url} />
               <p>
-                {data.intro_3}
+              {replaceLi(data.intro_3)}
               </p>
             </div>
           </div>
