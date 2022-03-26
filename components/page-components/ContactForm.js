25274735c58daaa39524bef225ac7e1d4daa4c7c
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2022-03-26 21:10:44 +0800

    form submits

--- components/page-components/ContactForm.js
+++ components/page-components/ContactForm.js
@@ -1,14 +1,26 @@
 // React
 import { useState } from "react";
 
+// Libs
+import axios from "axios";
+
 const ContactForm = () => {
+  // Submitting state
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+  const [submitError, setSubmitError] = useState("");
+
+  // Form state
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     phone: "",
     type: "individueel",
     message: "",
+    formMessage: "",
   });
+
+  console.log(formData)
 
   const updateForm = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -24,70 +36,147 @@ const ContactForm = () => {
     parent.classList.remove("focus");
   };
 
+  const submitForm = (e) => {
+    e.preventDefault();
+    setSubmitError(false);
+    setIsSubmitting(true)
+
+    if (formData.formMessage) {
+      setSubmitSuccess(false);
+      setSubmitError("Sorry er is iets mis gegaan, probeer het later nog een keer");
+      setIsSubmitting(false)
+      setTimeout(() => {
+        setSubmitError('')
+      }, 5000);
+      return;
+    }
+
+    delete formData.formMessage
+
+    const body = {
+      data: formData
+    };
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    axios
+      .post(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/contact-forms`,
+        body,
+        config
+      )
+      .then(() => {
+        setSubmitSuccess(true);
+      })
+      .catch(() => {
+        setSubmitError(
+          "Sorry, er is iets mis gegaan. Probeer het nog een keer of stuur me direct een email"
+        );
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
+  };
+
   return (
-    <form className="contact-form">
-      <div className="field">
-        <label htmlFor="name">Naam</label>
-        <input
-          required
-          type="text"
-          value={formData.name}
-          onChange={updateForm}
-          onFocus={focusForm}
-          onBlur={onBlur}
-          id="name"
-        />
-      </div>
-      <div className="field">
-        <label htmlFor="email">Email</label>
-        <input
-          required
-          type="text"
-          value={formData.email}
-          onChange={updateForm}
-          onFocus={focusForm}
-          onBlur={onBlur}
-          id="email"
-        />
-      </div>
-      <div className="field">
-        <label htmlFor="phone">Telefoon</label>
-        <input
-          required
-          type="tel"
-          value={formData.phone}
-          onChange={updateForm}
-          onFocus={focusForm}
-          onBlur={onBlur}
-          id="phone"
-        />
-      </div>
-      <div className="field">
-        <label htmlFor="type">Type</label>
-        <select
-          id="type"
-          value={formData.type}
-          onChange={updateForm}
-          onFocus={focusForm}
-          onBlur={onBlur}
-        >
-          <option value="individueel">Individueel</option>
-          <option value="team">Team</option>
-          <option value="zakelijk">Zakelijk</option>
-        </select>
-      </div>
-      <div className="field">
-        <label htmlFor="message">Bericht</label>
-        <textarea
-          id="message"
-          value={formData.message}
-          onChange={updateForm}
-          onFocus={focusForm}
-          onBlur={onBlur}
-        ></textarea>
-      </div>
-      <button type='submit' className="btn btn--primary">Verzenden</button>
-    </form>
+    <>
+      {submitSuccess ? (
+        <div className="contact-form">
+          <div className="submit-success">
+            <p>Je bericht is verstuurd! We nemen zo gauw mogelijk contact met je
+            op.</p>
+          </div>
+        </div>
+      ) : (
+        <form className="contact-form" onSubmit={submitForm}>
+          <div className="field">
+            <label htmlFor="name">Naam</label>
+            <input
+              required
+              type="text"
+              value={formData.name}
+              onChange={updateForm}
+              onFocus={focusForm}
+              onBlur={onBlur}
+              id="name"
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="email">Email</label>
+            <input
+              required
+              type="text"
+              value={formData.email}
+              onChange={updateForm}
+              onFocus={focusForm}
+              onBlur={onBlur}
+              id="email"
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="phone">Telefoon</label>
+            <input
+              type="tel"
+              value={formData.phone}
+              onChange={updateForm}
+              onFocus={focusForm}
+              onBlur={onBlur}
+              id="phone"
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="type">Type</label>
+            <select
+              id="type"
+              value={formData.type}
+              onChange={updateForm}
+              onFocus={focusForm}
+              onBlur={onBlur}
+            >
+              <option value="individueel">Individueel</option>
+              <option value="teams">Teams</option>
+              <option value="zakelijk">Zakelijk</option>
+            </select>
+          </div>
+          <div className="field">
+            <label htmlFor="message">Bericht</label>
+            <textarea
+              id="message"
+              value={formData.message}
+              onChange={updateForm}
+              onFocus={focusForm}
+              onBlur={onBlur}
+            ></textarea>
+          </div>
+          <div id="form-msg" className="field">
+            <label htmlFor="message">Bericht</label>
+            <textarea
+              id="formMessage"
+              value={formData.formMessage}
+              onChange={updateForm}
+              onFocus={focusForm}
+              onBlur={onBlur}
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="btn btn--primary"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Sending..." : "Verzenden"}
+          </button>
+        </form>
+      )}
+      {submitError && (
+        <div className="contact-form" style={{ marginTop: 12 }}>
+          <p style={{ color: "firebrick" }}>{submitError}</p>
+        </div>
+      )}
+    </>
   );
 };
 
--- components/ui-components/layout/Footer.js
+++ components/ui-components/layout/Footer.js
@@ -1,11 +1,64 @@
 // Next
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+
+// Libs
+import axios from "axios";
 
 // Content
 import Logo from "@assets/logos/logo_white.svg";
 
 const Footer = () => {
+  const [phone, setPhone] = useState("");
+  const [msg, setMsg] = useState("");
+  const [submitting, setSubmitting] = useState(false)
+  const [submitError, setSubmitError] = useState('')
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    setSubmitError('')
+    setSubmitting(true)
+    if (msg) {
+      setSubmitError(
+        "Sorry, er is iets mis gegaan. Probeer het later nog een keer of bel me direct!"
+      );
+      setSubmitting(false)
+      return;
+    }
+
+    const body = {
+      data: {
+        phone: phone,
+        returned_call: false,
+      },
+    };
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    axios
+      .post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/callbacks`, body, config)
+      .then(() => {
+        setSubmitSuccess(true);
+      })
+      .catch(() => {
+        setSubmitSuccess(false);
+        setSubmitError(
+          "Sorry, er is iets mis gegaan. Probeer het later nog een keer of bel me direct!"
+        );
+        setTimeout(() => {
+          setSubmitError('')
+        }, 5000);
+      }).finally(() => {
+        setSubmitting(false)
+      }) 
+  };
+
   return (
     <footer className="footer">
       <div className="container">
@@ -24,13 +77,29 @@ const Footer = () => {
           <div className="footer__grid__callback">
             <div>
               <span>Bel mij terug</span>
-              <p>
-                Laat hier je telefoonnummer achter en ik bel je terug. 
-              </p>
-              <form>
-                  <input type='text' placeholder="Bel mij terug" />
-                  <button type='submit'>Verzenden</button>
+              <p>Laat hier je telefoonnummer achter en ik bel je terug.</p>
+              {submitSuccess ? (
+                <div className="submit-success">
+                  <p>Je wordt zo gauw mogelijk terug gebeld!</p>
+                </div>
+              ) : (
+              <form onSubmit={submitForm}>
+                <input
+                  onChange={(e) => setPhone(e.target.value)}
+                  type="text"
+                  id="footer-tel"
+                  placeholder="Bel mij terug"
+                />
+                <input
+                  onChange={(e) => setMsg(e.target.value)}
+                  type="text"
+                  id="footer-msg"
+                  placeholder="Jou bericht"
+                />
+                <button type="submit" disabled={submitting}>{submitting ? 'Sending...' : 'Verzenden'}</button>
               </form>
+              )}
+              <div style={{color: 'firebrick', marginTop: 12}}>{submitError}</div>
             </div>
           </div>
         </div>
